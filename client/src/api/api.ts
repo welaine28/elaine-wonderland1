7be6080fa53acd0eb0a41aeea118087380a754f1
Yang Wang <elainewy@meta.com>
2025-05-27 23:35:27 -0700

    add model test1
    Signed-off-by: Yang Wang <elainewy@meta.com>

--- client/src/api/api.ts
+++ client/src/api/api.ts
@@ -1,10 +1,7 @@
 
 export const fetchMessage = async () => {
     try {
-      console.log("API_BASE", import.meta.env.VITE_API_BASE_URL)
-      console.log("Mode", import.meta.env.VITE_MODE)
-      console.log("fetching message, is prod",import.meta.env.PROD)
-      console.log("fetching message, is dev",import.meta.env.DEV)
+      console.log("url",`${import.meta.env.VITE_API_BASE_URL}/api/frontend/main`)
       const res = await fetch(`${import.meta.env.VITE_API_BASE_URL}/api/frontend/main`);
       console.log(res);
       return await res.text();
