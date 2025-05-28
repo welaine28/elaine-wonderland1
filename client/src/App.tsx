af36ebb9d692c42bde286e19acbb207fd01562a8
Yang Wang <elainewy@meta.com>
2025-05-27 20:39:38 -0700

    add model test1
    Signed-off-by: Yang Wang <elainewy@meta.com>

---
+++ client/src/App.tsx
@@ -0,0 +1,21 @@
+import './App.css';
+
+// client/src/App.tsx
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import NotFound from "./pages/NotFound";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
