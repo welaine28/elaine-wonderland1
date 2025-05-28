a8f532a25d753c993f0cbcb31c3942a30e98eecc
Yang Wang <elainewy@meta.com>
2025-05-27 23:58:15 -0700

    add model test1
    Signed-off-by: Yang Wang <elainewy@meta.com>

--- client/src/pages/About.tsx
+++ client/src/pages/About.tsx
@@ -1,4 +1,16 @@
+import { useEffect, useState } from "react";
+import { fetchTest } from "../api/api";
+
 // client/src/pages/About.tsx
 export default function About() {
-    return <h1>About Page</h1>;
+      const [data, setData] = useState({});
+      useEffect(() => {
+          fetchTest().then((data) => {
+              setData(data);
+          });
+          }, []);
+    return <div>
+            <h1>About Page</h1>;
+              <div>{JSON.stringify(data)}</div>
+         </div>
   }
