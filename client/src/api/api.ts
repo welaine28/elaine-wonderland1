76453e0165c212f9a7b82301de170a71b488c98f
Yang Wang <elainewy@meta.com>
2025-05-27 23:45:56 -0700

    add model test1
    Signed-off-by: Yang Wang <elainewy@meta.com>

--- client/src/api/api.ts
+++ client/src/api/api.ts
@@ -4,7 +4,7 @@ export const fetchMessage = async () => {
       console.log("url",`${import.meta.env.VITE_API_BASE_URL}/api/frontend/main`)
       const res = await fetch(`${import.meta.env.VITE_API_BASE_URL}/api/frontend/main`);
       console.log(res);
-      return await res.text();
+      return res.json();
     } catch (err) {
       console.error("Fetch failed:", err);
       throw err;
